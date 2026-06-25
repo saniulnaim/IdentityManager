ab7101cfb9add4357cc6a9e0b9a2509949b5fa0b
saniu <saniulnaim@gmail.com>
2026-06-25 12:27:04 +0600

    Assign Roles to User

--- IdentityManager/Controllers/UserController.cs
+++ IdentityManager/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityManager.Data;
 using IdentityManager.Models;
+using IdentityManager.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,11 +10,13 @@ namespace IdentityManager.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
-        public UserController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        public UserController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _context = context;
             _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         public IActionResult Index()
@@ -25,7 +28,7 @@ namespace IdentityManager.Controllers
             foreach (var user in userList)
             {
                 var role = userRole.FirstOrDefault(ur => ur.UserId == user.Id);
-                if(role == null)
+                if (role == null)
                 {
                     user.Role = "none";
                 }
@@ -37,5 +40,104 @@ namespace IdentityManager.Controllers
 
             return View(userList);
         }
+
+        public async Task<IActionResult> ManageRole(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user) as List<string>;
+            var model = new RolesViewModel()
+            {
+                User = user
+            };
+
+            foreach (var role in _roleManager.Roles)
+            {
+                RoleSelection roleSelection = new RoleSelection()
+                {
+                    RoleName = role.Name
+                };
+
+                if (userRoles.Any(r => r == role.Name))
+                {
+                    roleSelection.IsSelected = true;
+                }
+                model.Roles.Add(roleSelection);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManageRole(RolesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.User.Id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var oldUserRoles = await _userManager.GetRolesAsync(user);
+            var result = await _userManager.RemoveFromRolesAsync(user, oldUserRoles);
+
+            if (!result.Succeeded)
+            {
+                return View(result);
+            }
+
+            result = await _userManager.AddToRolesAsync(user, model.Roles.Where(a => a.IsSelected).Select(_ => _.RoleName));
+
+            if (!result.Succeeded)
+            {
+                return View(result);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LockUnlock(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            if(user.LockoutEnd != null && user.LockoutEnd > DateTime.Now) 
+            {
+                // User is locked and will remain locked untill lockout end time clicking on this action will unlock them
+                user.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                user.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteUser(string userId) 
+        {
+            var user = _context.ApplicationUsers.FirstOrDefault(u=> u.Id == userId);
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            _context.ApplicationUsers.Remove(user);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
+
---
+++ IdentityManager/Models/ViewModels/RolesViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace IdentityManager.Models.ViewModels
+{
+    public class RolesViewModel
+    {
+        public RolesViewModel()
+        {
+            Roles = new List<RoleSelection>();
+        }
+
+        public ApplicationUser User { get; set; }
+        public IList<RoleSelection> Roles { get; set; }
+    }
+
+    public class RoleSelection
+    {
+        public string RoleName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
